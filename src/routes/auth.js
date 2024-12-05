b1a571acbbb46f7722a9d0b02ee8919baa285a39
Pedro Thiago Roque <pedronmroque@gmail.com>
2024-12-05 02:57:12 -0300

    Correção de bug - cant find module chat com c minusculo

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Tela de Registro
 router.get('/register', (req, res) => res.render('register'));
--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Chat = require('../models/chat');
+const Chat = require('../models/Chat');
 require('dotenv').config();
 
 const OpenAI = require('openai');
